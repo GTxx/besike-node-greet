c6bd3b049a5af2af38d5faa992228952e85e3699
GTxx <xiongxiong1986@gmail.com>
2014-12-30 19:23:41 +0800

    add {} around if block

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var greet = function(name, drunk) {
     if (drunk){
-        if(name)
+        if(name) {
             return "hello " + name + " , you look sexy"
+        }
     } else {
-        if (name)
+        if (name) {
             return "hello " + name;
+        }
     }
     return "No name provided"
 }
